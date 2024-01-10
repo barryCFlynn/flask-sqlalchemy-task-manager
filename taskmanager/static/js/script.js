97368f0804728c284d0a2990e7c6138a4e0646ac
barryCFlynn <>
2024-01-10 14:58:07 +0000

    Add materialize css and flask templates

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // sudebave initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});
